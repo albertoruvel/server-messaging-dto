4c62b9e1470535013412fea96532f90fd2ffa0d4
Alberto <arubalcaba@24hourfit.com>
2017-07-13 11:41:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Needed transfer objects for all messaging projects
---
+++ src/main/java/com/messaging/dto/ErrorMessageRequest.java
@@ -0,0 +1,38 @@
+package com.messaging.dto;
+
+public class ErrorMessageRequest {
+    private String body;
+    private String date;
+    private String applicationId;
+
+    public ErrorMessageRequest(String body, String date, String applicationId) {
+        this.body = body;
+        this.date = date;
+        this.applicationId = applicationId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+
+}
